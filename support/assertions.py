1ab10144ccec759172f639768ed02b535d1947f2
Yana Kormshchikova <veryutina.yana@yandex.ru>
2023-11-04 18:10:00 +0300

    restore

--- support/assertions.py
+++
@@ -1,7 +0,0 @@
-from support.assertion_errors import AssertionErrors
-
-
-class Assertions:
-    @staticmethod
-    def assert_equal(actual, expected):
-        assert actual == expected, AssertionErrors.EQUAL.format(expected, actual)
